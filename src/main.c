aabfc218c9c6009722722ccc08b6f05d3dce0a76
lilendian0x00 <arianth82@gmail.com>
2023-03-22 17:27:24 -0500

    Fixed run command message

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        fprintf(stderr, "./%s <dns_server> <domain> <TCP, UDP>\n", argv[0]);
+        fprintf(stderr, "%s <dns_server> <domain> <TCP, UDP>\n", argv[0]);
         exit(1);
     }
     char DNS_SERVER[15];
